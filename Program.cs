21a7cae8e66bf9211c869bf604113af9042f2c4a
Chen Yu Pao <windperson@gmail.com>
2018-04-25 11:40:25 +0800

    in memory queue demo

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MassTransit;
+
+namespace MassiveTransitDemo
+{
+    public class YourMessage { public string Text { get; set; } }
+public class Program
+{
+    public static void Main()
+    {
+        var bus = Bus.Factory.CreateUsingInMemory(sbc =>
+        {
+            sbc.ReceiveEndpoint( "test_queue", ep =>
+            {
+                ep.Handler<YourMessage>(context =>
+                {
+                    return Console.Out.WriteLineAsync($"Received: \"{context.Message.Text}!\"");
+                });
+            });
+        });
+
+        bus.Start();
+
+        bus.Publish(new YourMessage{Text = "Hello MassiveTransit"});
+        bus.Publish(new YourMessage{Text = "In Memory Queue"});
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        bus.Stop();
+
+        }
+    }
+}
