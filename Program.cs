c62c2c4e5184e6349d2d886019940b82c997ba0e
Chen Yu Pao <windperson@gmail.com>
2018-04-26 13:09:41 +0800

    a better demo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using MassTransit;
 using MassTransit.Transports.InMemory;
@@ -7,7 +8,7 @@ namespace MassiveTransitDemo
 {
     public class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
             IHost inRamHost = null;
 
@@ -17,68 +18,62 @@ namespace MassiveTransitDemo
 
                 sbc.ReceiveEndpoint("test_queue1", ep =>
                 {
-                    ep.Handler<YourMessage>(context =>
-                    {
-                        Console.Out.WriteLineAsync($"Preset Handler Received: \"{context.Message.Text}!\"");
-                        return Task.CompletedTask;
-                    });
+                    ep.Handler<YourMessage>(async (context) =>
+                   {
+                       await Console.Out.WriteLineAsync($"Preset Handler Received: \"{context.Message.Text}!\"");
+                   });
                 });
             });
 
-
-
             bus.Start();
-
 
             HostReceiveEndpointHandle receiveEndpoint = null;
 
             if (inRamHost != null && inRamHost is IInMemoryHost host)
             {
-                receiveEndpoint = host.ConnectReceiveEndpoint("test_queue2", (configurator) =>
+                receiveEndpoint = host.ConnectReceiveEndpoint("test_queue2", configurator =>
                 {
-                    configurator.Handler<YourMessage>(context =>
-                    {
-                        Console.Out.WriteLineAsync($"Dynamic Endpoint Received: \"{context.Message.Text}!\"");
-                        return Task.CompletedTask;
-                    });
+                    configurator.Handler<YourMessage>(async context =>
+                   {
+                       await Console.Out.WriteLineAsync($"Dynamic Endpoint Received: \"{context.Message.Text}!\"");
+                   });
                 });
             }
 
-            var endpoint1 = bus.GetSendEndpoint(new Uri("loopback://localhost/test_queue1")).Result;
+            var endpoint1 = await bus.GetSendEndpoint(new Uri("loopback://localhost/test_queue1"));
+            var endpoint2 = await bus.GetSendEndpoint(new Uri("loopback://localhost/test_queue2"));
 
-            endpoint1.Send<YourMessage>(new YourMessage {Text = "Hello MassiveTransit"});
-            endpoint1.Send<YourMessage>(new YourMessage { Text = "Send again" });
+            await Task.WhenAll(
+                endpoint1.Send<YourMessage>(new YourMessage { Text = "Hello MassiveTransit" }),
+                endpoint1.Send<YourMessage>(new YourMessage { Text = "Send again" }),
+                endpoint2.Send<YourMessage>(new YourMessage { Text = "Hello MassiveTransit" }),
+                endpoint2.Send<YourMessage>(new YourMessage { Text = "Send again" }));
 
-            var endpoint2 = bus.GetSendEndpoint(new Uri("loopback://localhost/test_queue2")).Result;
+            await Task.WhenAll(
+                bus.Publish(new YourMessage { Text = "[1st] BroadCasting!" }),
+                bus.Publish(new YourMessage { Text = "[2nd] BroadCasting again!!" }));
 
-            endpoint2.Send<YourMessage>(new YourMessage { Text = "Hello MassiveTransit" });
-            endpoint2.Send<YourMessage>(new YourMessage { Text = "Send again" });
+            Thread.Sleep(new TimeSpan(0,0,1));
 
+            await receiveEndpoint?.StopAsync();
 
-            bus.Publish(new YourMessage { Text = "BroadCasting!!!" });
+            await bus.Publish(new YourMessage { Text = "[3rd] Final BroadCasting!!!" });
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
 
-            receiveEndpoint?.StopAsync();
-
-            bus.StopAsync();
-
+            await bus.StopAsync();
         }
     }
-
 
     public class YourMessage { public string Text { get; set; } }
 
-
-    public class MyConsumer : IConsumer<YourMessage>
-    {
-#pragma warning disable 1998
-        public async Task Consume(ConsumeContext<YourMessage> context)
-#pragma warning restore 1998
-        {
-            Console.WriteLine($"Text={context.Message.Text}");
-        }
-    }
+    // public class MyConsumer : IConsumer<YourMessage>
+    // {
+    //     public async Task Consume(ConsumeContext<YourMessage> context)
+    //     {
+    //        await Console.Out.WriteLineAsync($"Text={context.Message.Text}");
+    //     }
+    // }
 
 }
